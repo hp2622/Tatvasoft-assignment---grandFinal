edd5110c68937fbb8ec8f09f92e88f336638e69c
hp2622 <harshpatel26022002@gmail.com>
2023-01-31 09:19:26 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let menuIcon = document.querySelector("#menu-icon");
+
+let downArrow = document.querySelector("#down-arrow");
+
+let leftSidebar = document.querySelector(".left--sidebar");
+
+
+
+menuIcon.addEventListener("click", () => {
+    if(leftSidebar.classList.contains('open')){
+        leftSidebar.classList.remove('open');
+        leftSidebar.classList.add('close');
+    }
+
+
+    else if(leftSidebar.classList.contains('close')){
+        leftSidebar.classList.remove('close');
+        leftSidebar.classList.add('open');
+    }
+})
